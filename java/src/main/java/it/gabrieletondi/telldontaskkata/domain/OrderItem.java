66b936ed354d51acc7e4af41f5b749e6344e2b0c
Henning Langhorst <henning.langhorst@gmail.com>
2023-05-21 00:09:54 +0200

    remove setters and add constructor

--- java/src/main/java/it/gabrieletondi/telldontaskkata/domain/OrderItem.java
+++ java/src/main/java/it/gabrieletondi/telldontaskkata/domain/OrderItem.java
@@ -6,54 +6,40 @@ import static java.math.BigDecimal.valueOf;
 import static java.math.RoundingMode.HALF_UP;
 
 public class OrderItem {
-    private Product product;
-    private int quantity;
-    private BigDecimal taxedAmount;
-    private BigDecimal tax;
+    private final Product product;
+    private final int quantity;
+    private final BigDecimal taxedAmount;
+    private final BigDecimal tax;
+
+    public OrderItem(Product product, int quantity, BigDecimal taxedAmount, BigDecimal tax) {
+        this.product = product;
+        this.quantity = quantity;
+        this.taxedAmount = taxedAmount;
+        this.tax = tax;
+    }
 
     public static OrderItem from(Product product, int quantity) {
         final BigDecimal unitaryTax = product.getPrice().divide(valueOf(100)).multiply(product.getCategory().taxPercentage()).setScale(2, HALF_UP);
         final BigDecimal unitaryTaxedAmount = product.getPrice().add(unitaryTax).setScale(2, HALF_UP);
         final BigDecimal taxedAmount = unitaryTaxedAmount.multiply(BigDecimal.valueOf(quantity)).setScale(2, HALF_UP);
         final BigDecimal taxAmount = unitaryTax.multiply(BigDecimal.valueOf(quantity));
-
-        final OrderItem orderItem = new OrderItem();
-        orderItem.setProduct(product);
-        orderItem.setQuantity(quantity);
-        orderItem.setTax(taxAmount);
-        orderItem.setTaxedAmount(taxedAmount);
-        return orderItem;
+        return new OrderItem(product, quantity, taxedAmount, taxAmount);
     }
 
     public Product getProduct() {
         return product;
     }
 
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
     public int getQuantity() {
         return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
     }
 
     public BigDecimal getTaxedAmount() {
         return taxedAmount;
     }
 
-    public void setTaxedAmount(BigDecimal taxedAmount) {
-        this.taxedAmount = taxedAmount;
-    }
-
     public BigDecimal getTax() {
         return tax;
     }
 
-    public void setTax(BigDecimal tax) {
-        this.tax = tax;
-    }
 }
