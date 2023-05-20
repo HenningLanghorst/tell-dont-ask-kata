eebe3b0ec173a11cd669acfacdcf5197b60d88ba
Henning Langhorst <henning.langhorst@gmail.com>
2023-05-20 23:46:41 +0200

    move code to Order.markAsShipped

--- java/src/main/java/it/gabrieletondi/telldontaskkata/domain/Order.java
+++ java/src/main/java/it/gabrieletondi/telldontaskkata/domain/Order.java
@@ -1,8 +1,13 @@
 package it.gabrieletondi.telldontaskkata.domain;
 
+import it.gabrieletondi.telldontaskkata.useCase.OrderCannotBeShippedException;
+import it.gabrieletondi.telldontaskkata.useCase.OrderCannotBeShippedTwiceException;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+
+import static it.gabrieletondi.telldontaskkata.domain.OrderStatus.*;
 
 public class Order {
     private BigDecimal total;
@@ -100,5 +105,19 @@ public class Order {
 
         setTotal(getTotal().add(orderItem.getTaxedAmount()));
         setTax(getTax().add(orderItem.getTax()));
+    }
+
+    public void verifyIsShippable() {
+        if (getStatus().equals(CREATED) || getStatus().equals(REJECTED)) {
+            throw new OrderCannotBeShippedException();
+        }
+
+        if (getStatus().equals(SHIPPED)) {
+            throw new OrderCannotBeShippedTwiceException();
+        }
+    }
+
+    public void markAsShipped() {
+        setStatus(SHIPPED);
     }
 }
